612dbd482824ae92e7bc8fe8ecc0d69d899289c8
zhr211 <zhr211@4261811f904c>
2019-01-20 19:10:41 +0000

    Models tutorial complete

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3l%3sn0odba)iw@_#%=_2!9+@kadv@(2ild#ylc-mz!f-*%(s8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
